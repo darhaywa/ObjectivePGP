e8a168fa9f96e6c19c775606c5eecc859e35df3a
Daren Hayward <darhaywa@synamedia.com>
2020-03-30 10:37:51 +0100

    Added missing targets argument

--- Package.swift
+++ Package.swift
@@ -4,6 +4,10 @@ import PackageDescription
 let package = Package(
     name: "ObjectivePGP",
     products: [
-        .library(name: "ObjectivePGP")
+        .library(name: "ObjectivePGP", targets: ["ObjectivePGP"])
+    ],
+    targets: [ .target(name: "ObjectivePGP",
+                       dependencies: [],
+                       path: "ObjectivePGP")
     ]
 )
